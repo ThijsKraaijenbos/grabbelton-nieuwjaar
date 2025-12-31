98cae4cd76c0f664f6932eab62c9013f3a7c4389
Thijs Kraaijenbos <47988622+ThijsKraaijenbos@users.noreply.github.com>
2025-12-31 17:01:10 +0100

    build fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-	base: './', // crucial for GitHub Pages
+	base: '/grabbelton-nieuwjaar/', // Replace with YOUR repository name
 	plugins: [react()],
 });
